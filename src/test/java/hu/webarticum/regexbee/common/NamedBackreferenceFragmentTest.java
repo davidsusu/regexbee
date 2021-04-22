b225ebf9dbc714bc9751fcda3f143633545f1fa3
Horváth Dávid <horvath.david@telekom.hu>
2021-04-22 06:47:56 +0200

    Fix test class name

--- src/test/java/hu/webarticum/regexbee/common/NamedBackreferenceFragmentTest.java
+++ src/test/java/hu/webarticum/regexbee/common/NamedBackreferenceFragmentTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-class NamedBackreferenceTest {
+class NamedBackreferenceFragmentTest {
 
     @Test
     void testValidName() {
